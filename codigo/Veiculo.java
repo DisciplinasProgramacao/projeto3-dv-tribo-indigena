4544fb66c51915ebd0b6e8cf1f4c64290c61738b
christiano <christianoruvieri90@gmail.com>
2023-10-17 13:51:18 +0000

    Commit veiculo

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,21 +1,54 @@
+import java.util.Scanner;
+import java.time.LocalTime;
+import java.time.Duration;
+
+
+
 public class Veiculo {
 
     private String placa;
     private UsoDeVaga[] usos;
+    public Scanner teclado = new Scanner(System.in);
+    public LocalTime time1;
+    public  LocalTime time2;
+    public  Duration duration;
+    public long totalMinutes;
+    public long blocksOf15Minutes;
+    public long cost;
 
+   public String getPlaca;
     public Veiculo(String placa) {
+        System.out.print("Escreva a Placa do carro:");
+        getPlaca= teclado.nextLine();
 
     }
 
     public void estacionar(Vaga vaga) {
+System.out.print("Digite a vaga no formato 8y:(8-numero da vaga, y-fileira da vaga)");
+vaga = teclado.nextLine();
+        System.out.println("Insira o horario do inicio da estadia no estacionamento:(formato HH:mm):");
+        time1 = LocalTime.parse(teclado.nextLine());
+
 
     }
 
     public double sair() {
+        System.out.println("Insira o horario de saida do estacionamento:(formato HH:mm):");
+        time2 = LocalTime.parse(teclado.nextLine());
+
 
     }
 
     public double totalArrecadado() {
+        Duration duration = Duration.between(time1, time2);
+        long totalMinutes = duration.toMinutes();
+        long blocksOf15Minutes = (totalMinutes + 14) / 15;
+        long cost = blocksOf15Minutes * 4;
+        if (cost > 50) {
+            cost = 50;
+        }
+        System.out.println("Custo da estadia: R$" + cost);
+
 
     }
 
@@ -24,6 +57,10 @@ public class Veiculo {
     }
 
     public int totalDeUsos() {
+
+    }
+
+    private static void main(String[] args){
 
     }
 
