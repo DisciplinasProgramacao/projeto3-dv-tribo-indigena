9c71427b21d26adcb2022dc7c3a1d89af1903041
Pedro <pedrolfirbida@gmail.com>
2023-10-24 22:43:53 +0000

    Update Estacionamento5m.java

--- codigo/Estacionamento5m.java
+++ codigo/Estacionamento5m.java
@@ -80,46 +80,4 @@ public class Estacionamento {
         double valor = tempoOcupada * 4.0;
         return Math.min(valor, 50.0); // Valor máximo de R$50
     }
-
-    public double totalArrecadado() {
-        double total = 0;
-        for (int i = 0; i < quantFileiras; i++) {
-            for (int j = 0; j < vagasPorFileira; j++) {
-                if (vagas[i][j].isOcupada()) {
-                    total += calcularCobranca(vagas[i][j].getHoraEntrada());
-                }
-            }
-        }
-        return total;
-    }
-
-    public double arrecadacaoNoMes(int mes) {
-        double total = 0;
-        for (int i = 0; i < quantFileiras; i++) {
-            for (int j = 0; j < vagasPorFileira; j++) {
-                if (vagas[i][j].isOcupada() && vagas[i][j].getVeiculo().getMesEstacionado() == mes) {
-                    total += calcularCobranca(vagas[i][j].getHoraEntrada());
-                }
-            }
-        }
-        return total;
-    }
-
-    public double valorMedioPorUso() {
-        if (clientes.isEmpty()) {
-            return 0;
-        }
-        double totalArrecadado = totalArrecadado();
-        int totalClientes = clientes.size();
-        return totalArrecadado / totalClientes;
-    }
-
-    public String top5Clientes(int mes) {
-        clientes.sort((c1, c2) -> Double.compare(c2.arrecadacaoNoMes(mes), c1.arrecadacaoNoMes(mes));
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < Math.min(5, clientes.size()); i++) {
-            result.append(i + 1).append(". ").append(clientes.get(i).getNome()).append(": R$").append(clientes.get(i).arrecadacaoNoMes(mes)).append("\n");
-        }
-        return result.toString();
-    }
 }
