5d06508eab94ff6faf59e6b1cac65ec9e4c21290
ceolin <gustavo@al3xdev.com>
2023-10-25 14:16:26 +0000

    Update UsoDeVagas.java

--- codigo/UsoDeVagas.java
+++ codigo/UsoDeVagas.java
@@ -1,40 +1,83 @@
+package br.pucminas.titas.entidades;
+
+import br.pucminas.titas.excecoes.VeiculoJaSaiuException;
+import br.pucminas.titas.excecoes.ServicoNaoTerminadoException;
+import br.pucminas.titas.excecoes.VagaNaoDisponivelException;
+import br.pucminas.titas.enums.Servico;
+
+import java.io.Serializable;
 import java.time.Duration;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
-class UsoDeVaga {
-    private String vaga;
-    private LocalTime horarioInicio;
-    private LocalTime horarioTermino;
+public class UsoDeVaga implements Serializable {
 
+	private static final double VALOR_FRACAO = 4.0;
+	private static final double VALOR_MAXIMO = 50.0;
+	private Vaga vaga;
+	private LocalDateTime entrada;
+	private LocalDateTime saida;
+	private double valorPago;
+	private Servico servico;
 
-    public UsoDeVaga(String vaga, LocalTime horarioInicio) {
-        this.vaga = vaga;
-        this.horarioInicio = horarioInicio;
-    }
+	public UsoDeVaga(Vaga vaga) throws VagaNaoDisponivelException {
+		this(vaga, null);
+	}
 
-    public void finalizarUso() {
-        this.horarioTermino = LocalTime.now();
-    }
-    public double sair() {
-        if (null == null) {
-            System.out.println("O veículo não está estacionado!");
-            return 0;
-        }
-        this.finalizarUso();
-        //double custo = calcularCusto(this.getDuracao()) + servicos.getTotalServiceCost();
-        //return custo;
-        return 2;
-    }
+	public UsoDeVaga(Vaga vaga, Servico servico) throws VagaNaoDisponivelException {
+		if (!vaga.disponivel()) {
+			throw new VagaNaoDisponivelException("Vaga não disponível");
+		}
+		this.vaga = vaga;
+		this.servico = servico;
+		this.entrada = LocalDateTime.now();
+	}
 
-    public Duration getDuracao() {
-        if (horarioTermino == null) return Duration.ZERO;
-        return Duration.between(horarioInicio, horarioTermino);
-    }
-    public double calcularCusto(Duration duracao) {
-        long totalMinutes = duracao.toMinutes();
-        long blocksOf15Minutes = (totalMinutes + 14) / 15;
-        long cost = blocksOf15Minutes * 4;
-        return (cost > 50) ? 50 : cost;
+	public LocalDateTime getEntrada() {
+		return entrada;
+	}
 
-    }
-    public JSONObject getObj(){         JSONObject UsoDeVaga= new JSONObject();         UsoDeVaga.put("Custo", this.calcularCusto);      }
+	public double sair() throws ServicoNaoTerminadoException, VeiculoJaSaiuException {
+		if (saida != null) {
+			throw new VeiculoJaSaiuException();
+		}
+		if (!podeSair(LocalDateTime.now())) {
+			throw new ServicoNaoTerminadoException("Os serviços solicitados ainda não foram concluídos");
+		}
+		saida = LocalDateTime.now();
+		vaga.sair();
+		return calcularValorPago();
+	}
+
+	private double calcularValorPago() {
+		if (saida == null) {
+			return 0;
+		}
+		Duration duration = Duration.between(entrada, saida);
+		double minutos = duration.toMinutes();
+		valorPago = Math.floor(minutos / 15) * VALOR_FRACAO;
+
+		if (valorPago > VALOR_MAXIMO) {
+			valorPago = VALOR_MAXIMO;
+		}
+		if (servico != null) {
+			valorPago += servico.getPreco();
+		}
+		return valorPago;
+	}
+
+	public void adicionaServico(Servico servico) {
+		this.servico = servico;
+	}
+
+	public double getServicoPrecoTotal() {
+		return servico.getPreco();
+	}
+
+	public boolean podeSair(LocalDateTime saida) {
+		if (servico == null) {
+			return true;
+		}
+		Duration duration = Duration.between(entrada, saida);
+		return duration.toHours() >= servico.getHorasMinimas();
+	}
+}
